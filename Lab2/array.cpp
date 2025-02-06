cc1167fc6698cd32b4455431a6b269de8355428b
LeoB-Le <leobaoliemle@gmail.com>
2025-02-06 15:23:03 -0800

    a

--- Lab2/array.cpp
+++ Lab2/array.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 /*
+* @brief prints the elements 
+*
+ @param arr - array of intergers
+ * @param size - ther numbero f elements oin the array
+ */
 
-* @brief prints the elements */
+void printMemArr(const int * arr, int size) 
+{// void printMemArr(int arr[], int size)
+printf ("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
+for(int i = 0; i < size; i++)
+{
+    printf("Value: %i at Memory location: %p\n", arr[i], arr+i);
+}
+}
